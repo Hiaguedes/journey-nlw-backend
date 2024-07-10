8c321413064f697999eac1071c655c3f971fb8ab
Hiago Guedes <hiago@contasimples.com>
2024-07-10 00:02:25 -0300

    feat(NLW-aula-1): confeccao das primeiras rotas, cadastro de viagens, participantes e comeco do link de confirmacao

---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+# Journey Next Level Week - Node.js
+
+## Versao do node usada
+
+Node 20
+
+## Comandinhos que salvam na criacao dos projetos
+
+```bash
+    npx tsc --init
+    npx gitignore node
+    npx prisma init --datasource-provider SQLite # pra criar o time do datasource
+    npx prisma migrate dev # pra criar as tabelas
+    npx prisma studio # roda no navegador os registor que voce for criando
+```
---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import fastify from "fastify";
+import { prisma } from './lib/prisma'
+import { createTrips } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+    origin: 'http//localhost:4000'
+})
+app.register(createTrips)
+app.register(confirmTrip)
+
+app.get('/hello', () => {
+    return {
+        message: 'Hello!'
+    }
+})
+
+app.get('/listar', async () => {
+    const trips = await prisma.trip.findMany();
+
+    return trips
+})
+
+app.get('/cadastrar', async () => {
+    await prisma.trip.create({
+        data: {
+            destination: 'Rio de Janeiro',
+            starts_at: new Date(),
+            ends_at: new Date(),
+        }
+    })
+
+    return {
+        status: 200,
+        message: 'Viagem cadastrada com sucesso!'
+    }
+})
+
+app.listen({ port: 4000 }).then(() => {
+    console.log('server running')
+})
