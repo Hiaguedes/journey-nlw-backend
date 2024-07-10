c831e1c5c0684984281d0e3f562d00c9032cbfe1
Hiago Guedes <hiago@contasimples.com>
2024-07-10 03:45:14 -0300

    feat: implementa rota de confirmacao de viagem

---
+++ src/lib/dayJsHelper.ts
@@ -0,0 +1,8 @@
+import "dayjs/locale/pt-br"
+import dayjs from "dayjs";
+
+dayjs.locale('pt-br')
+
+const dayJsHelper = dayjs
+
+export { dayJsHelper }
--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -74,13 +74,13 @@ export const createTrips = async (app: FastifyInstance) => {
             html: `
                 <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
                     <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
-                    <p></p>
+                    <br />
                     <p>Para confirmar sua viagem, clique no link abaixo:</p>
-                    <p></p>
+                    <br />
                     <p>
                         <a href="${confirmationLink}">Confirmar viagem</a>
                     </p>
-                    <p></p>
+                    <br />
                     <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
                 </div>
             `.trim()
