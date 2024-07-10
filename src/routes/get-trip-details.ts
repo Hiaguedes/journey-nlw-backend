2acc7c3678610911647fe1f773d8100e86d2d2d4
Hiago Guedes <hiago@contasimples.com>
2024-07-10 20:12:55 -0300

    feat: cria rota de get trip details

---
+++ src/routes/get-trip-details.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from 'zod'
+import { prisma } from "../lib/prisma";
+import "dayjs/locale/pt-br"
+import dayjs from "dayjs";
+
+dayjs.locale('pt-br')
+
+export const getTripDetails = async (app: FastifyInstance) => {
+    app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId', {
+        schema: {
+            params: z.object({
+                tripId: z.string().uuid(),
+            }),
+
+        }
+    }, async (request) => {
+        const { tripId } = request.params;
+
+        const trip = await prisma.trip.findUnique({
+            where: { id: tripId },
+            select: {
+                id: true,
+                destination: true,
+                starts_at: true,
+                ends_at: true,
+                is_confirmed: true,
+            }
+        })
+
+        if (!trip) throw new Error('Trip not found on get trip details route')
+
+        return {
+            trip,
+        }
+    })
+}
